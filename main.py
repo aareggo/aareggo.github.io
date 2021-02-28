b7ab5684313b865ef5ce82366cbe71b59016ca7e
aareggo <aareggo@mail.ru>
2021-02-28 17:37:07 +0500

    nachali

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import json
+import requests
 import telebot
 
 bot = telebot.TeleBot('1584283406:AAHolV6-eANqsIHCx2zYVPGCOYZbWR_8NxM')
 
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(
+QIWI_TOKEN = ''
+QIWI_ACCOUNT = ''
 
-        message.chat.id, 'Привет, ты написал мне /start')
-
-
-bot.polling()
+s = requests.Session()
+s.headers['authorization'] = 'Bearer ' + QIWI_TOKEN
+parameters = {'rows': '50'}
+h = s.get('https://edge.qiwi.com/payment-history/v1/persons/' +
+          QIWI_ACCOUNT + '/payments', params=parameters)
+req = json.loads(h.text)
