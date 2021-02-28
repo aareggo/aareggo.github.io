d12a18efc6ebf8bd2359cff2ec39d1782f0f1cc8
aareggo <aareggo@mail.ru>
2021-02-28 17:24:20 +0500

    Three

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot('1584283406:AAHolV6-eANqsIHCx2zYVPGCOYZbWR_8NxM')
 def start_message(message):
     bot.send_message(
 
-        message.chat.id, 'Привет, ты написал мне /start , всем спс bro')
+        message.chat.id, 'Привет, ты написал мне /start')
 
 
 bot.polling()
