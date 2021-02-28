297437be6630a6c6b513342c431c5f7f44337b62
aareggo <aareggo@mail.ru>
2021-02-28 16:04:11 +0500

    test masse

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot('1584283406:AAHolV6-eANqsIHCx2zYVPGCOYZbWR_8NxM')
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(
-        message.chat.id, 'Привет, ты написал мне /startsss, poka')
+        message.chat.id, 'Привет, ты написал мне /starts')
 
 
 bot.polling()
